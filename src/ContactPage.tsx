3e129afe3f5985690158ac3c830f9e19e0167c3d
Seth Hobson <sethlawrencehobson@gmail.com>
2024-02-12 03:12:58 -0500

    added main components

---
+++ src/ContactPage.tsx
@@ -0,0 +1,11 @@
+function ContactPage() {
+
+
+    return (
+        <>
+        </>
+    )
+}
+
+
+export default ContactPage;
---
+++ src/RoutesList.tsx
@@ -0,0 +1,17 @@
+
+/**
+ * props: nothing
+ *
+ * State: none
+ *
+ * renders { ContactPage, HomePage, AboutPage, Resume }
+ */
+function RoutesList() {
+
+    return (
+        <>
+        </>
+    )
+}
+
+export default RoutesList;
